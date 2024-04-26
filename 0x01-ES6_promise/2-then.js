e5496dd9b5349ac95b4253f280bc26608decfb10
matendecherej <matendecherej@gmail.com>
2024-04-27 02:26:36 +0300

    3 handlers

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,25 +1,6 @@
-export default function getResponseFromAPI(argument) {
-  return new Promise((resolve, reject) => {
-    if (argument === true) {
-      const response = {
-        status: 200,
-        body: 'Success'
-      };
-      resolve(response);
-    } else {
-      const error = new Error('The fake API is not working currently');
-      reject(error);
-    }
-  })
-    .then(response => {
-      console.log('Got a response from the API');
-      return {
-        status: 200,
-        body: 'success'
-      };
-    })
-    .catch(error => {
-      console.log('Got an error from the API');
-      return new Error();
-    });
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
